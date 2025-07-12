64e662e7408257f86fce3105532a139860fa788b
ecthon <ecthon@gmail.com>
2025-07-12 13:42:33 -0400

    Refatora o componente Heading para melhorar a responsividade e a estrutura do layout, ajustando tamanhos de texto e classes de estilo, além de garantir que a imagem se adapte melhor em diferentes tamanhos de tela.

--- src/components/home/heading.tsx
+++ src/components/home/heading.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 
 export function Heading() {
   return (
-    <div className="flex items-center justify-center h-[400px] w-full max-w-[1248px] mx-auto space-y-4 px-4 sm:px-6 lg:px-8 bg-amber-50 rounded-3xl">
+    <div className="flex flex-col-reverse md:flex-row items-center justify-center min-h-[400px] w-full max-w-[1248px] mx-auto space-y-4 px-4 sm:px-6 lg:px-8 bg-amber-50 rounded-3xl py-8">
       {/* Left Side */}
-      <div className="flex w-1/2 flex-col justify-center gap-4">
-        <h1 className="text-4xl font-bold text-black">Planejando suas férias interestelares?🪐</h1>
-        <p className="text-md text-zinc-500">Alugue uma casinha futurista em qualquer canto da galáxia — suas férias interplanetárias começam aqui!</p>
+      <div className="flex w-full md:w-1/2 flex-col justify-center gap-4 text-center md:text-left">
+        <h1 className="text-3xl md:text-4xl font-bold text-black">Planejando suas férias interestelares?🪐</h1>
+        <p className="text-sm md:text-md text-zinc-500">Alugue uma casinha futurista em qualquer canto da galáxia — suas férias interplanetárias começam aqui!</p>
       </div>
 
       {/* Right Side */}
-      <div className="flex w-1/2 justify-center items-center">
-        <Image src="/heading-img.png" alt="House" width={350} height={350} />
+      <div className="flex w-full md:w-1/2 justify-center items-center">
+        <Image src="/heading-img.png" alt="House" width={350} height={350} className="w-64 h-64 md:w-auto md:h-auto" />
       </div>
     </div>
   );
